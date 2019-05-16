15792034f6ddeec328e885c5afc5da03b4642b62
fangxiaopeng <850899969@qq.com>
2019-05-16 15:24:42 +0800

    提交使用示例

--- app/src/main/java/com/fxp/codescanning/MainActivity.java
+++ app/src/main/java/com/fxp/codescanning/MainActivity.java
@@ -1,7 +1,13 @@
 package com.fxp.codescanning;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.fxp.libzxing.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.code_scan_txt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent("com.fxp.libzxing.CaptureActivity"), 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == CaptureActivity.QRCODE_REUSLT){
+            if (data == null) return;
+            String str = data.getStringExtra(CaptureActivity.EXTRA_RESULT);
+            if (str != null && !str.isEmpty()){
+                Toast.makeText(this, str, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
